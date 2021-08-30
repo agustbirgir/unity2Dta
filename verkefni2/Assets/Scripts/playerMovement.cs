b46d3ed6d906d6c9d8559d9639708d9abedcafb8
agustbirgir <54802234+agustbirgir@users.noreply.github.com>
2021-08-30 17:02:26 +0000

    begin project
    bjo til project

---
+++ verkefni2/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
